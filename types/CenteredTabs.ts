edc9cd3c274c06d691274d0712452935917d40e3
Joseph Claus <joseph.s.claus@gmail.com>
2021-07-26 14:58:24 -0400

    Move types, fix styling of homepage

--- types/CenteredTabs.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export interface CenteredTabsProps {
-    childrenList: {
-        [id: string]: {
-            component: React.ReactNode;
-        }
-    }
-}
--- types/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './DisplayTable';
-export * from './CenteredTabs';
-export * from './Commands';
-export * from './Header';
-export * from './ReduxItems';
