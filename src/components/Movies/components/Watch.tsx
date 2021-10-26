395e8bf91f323fef22f4c7db4ed4ab1914001253
Joseph Claus <joseph.s.claus@gmail.com>
2021-10-25 22:01:15 -0400

    Revert "Attempt to use https with hlsjs player"
    This reverts commit 5d5950eb5ffc907e6af96a26145f8a13a127bc7e.

--- src/components/Movies/components/Watch.tsx
+++ src/components/Movies/components/Watch.tsx
@@ -12,30 +12,20 @@ const Watch: React.FC<{ auth: User | null }> = (auth = null) => {
     useEffect(() => {
         if (auth !== null && !player) {
             //@ts-ignore
-            // const sldpPlayer = SLDP.init({
-            //     container:           'player',
-            //     stream_url:          "wss://planetexpressmovie.ddns.net:8081/live/spl-movies",
-            //     splash_screen:       `img/splash_screen.png`,
-            //     buffering:           1000,
-            //     sync_buffer:         4000,
-            //     controls:            true,
-            //     autoplay:            false,
-            //     key_frame_alignment: true,
-            //     reconnects:          100,
-            //     width:   'parent',
-            //     height:  'parent',
-            // });
-            // setPlayer(sldpPlayer);
-            const video = document.getElementById('hsljs-video-tag');
-            //@ts-ignore
-            const hls = new Hls();
-            hls.loadSource("https://planetexpressmovie.ddns.net/live/spl-movies/playlist.m3u8");
-            hls.attachMedia(video);
-            //@ts-ignore
-            hls.on(Hls.Events.MANIFEST_PARSED,function() {
-                //@ts-ignore
-                video.play();
+            const sldpPlayer = SLDP.init({
+                container:           'player',
+                stream_url:          "wss://planetexpressmovie.ddns.net:8081/live/spl-movies",
+                splash_screen:       `img/splash_screen.png`,
+                buffering:           1000,
+                sync_buffer:         4000,
+                controls:            true,
+                autoplay:            false,
+                key_frame_alignment: true,
+                reconnects:          100,
+                width:   'parent',
+                height:  'parent',
             });
+            setPlayer(sldpPlayer);
         }
     }, [auth]);
 
@@ -45,7 +35,7 @@ const Watch: React.FC<{ auth: User | null }> = (auth = null) => {
                 <p>
                     Double Click To Enter Fullscreen!
                 </p>
-                <video id='hsljs-video-tag'></video>
+                <div id="player"></div>
             </div>
         );
     } else return null;
