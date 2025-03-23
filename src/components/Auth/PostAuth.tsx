301d55e33387a8759eb1379433205ad1f9c3274f
Joseph Claus <ice2morrow@gmail.com>
2025-03-23 13:30:42 -0400

    Prevent manual navigation to /postAuth

--- src/components/Auth/PostAuth.tsx
+++ src/components/Auth/PostAuth.tsx
@@ -1,14 +1,21 @@
 import React, { useEffect } from 'react';
 
 import './PostAuth.scss';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../state/store';
+import { useNavigate } from 'react-router';
 
 const ORIGIN_PROTOCOL = window.location.protocol;
 const ORIGIN_HOST = window.location.host;
 const ORIGIN = `${ORIGIN_PROTOCOL}//${process.env.NODE_ENV === 'development' ? 'localhost:3000' : ORIGIN_HOST}`;
 
 const PostAuth: React.FC = () => {
+    const history = useNavigate();
+    const userFetchState = useSelector((state: RootState) => state.user.userFetchState);
 
     useEffect(() => {
+        if (userFetchState !== 'pending') history('/');
+
         window.addEventListener('message', (event) => {
             if (event.origin !== ORIGIN) return;
             const data = JSON.parse(event.data);
